4589fb334e077b62fad0c9dccd7548d3a799797a
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-08 23:32:33 +0530

    variables

---
+++ 1_Basics/2_Variables.js
@@ -0,0 +1,7 @@
+const accountId=12368465
+let accountName="Abcpvt ltd"
+var accountType="Private"
+console.log(accountId)
+// accountId=2121221  not possible
+console.log(accountId)  
+console.table([accountId,accountName,accountType])
