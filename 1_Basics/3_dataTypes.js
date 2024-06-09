9d1924050b18ccdf520023c9d6ca981b3139b7a0
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-09 10:30:56 +0530

    datatypes

---
+++ 1_Basics/3_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict";//treat all js as a newer version
+//alert("hello") //we are using nodejs not browser
+
+console.log(3
+    +
+    2);//not preffred , code readability should be high
+console.log("Radhe radhe");
+
+
+let name="sarvjeet" //string data type
+let age=18//NUmber
+let isLoggedIn=false //boolean
+
+
+/* 
+premitive data types
+
+
+number :- 2 to power 53
+bigint
+string =>" "
+boolean=>true/false
+null=>standard value
+undefined=>not assigned any value
+symbol=>unique*/
+
+
+//object
+
+console.log(typeof "ram")
+console.log(typeof(null))//object
+console.log(typeof(undefined))//undefined
+
