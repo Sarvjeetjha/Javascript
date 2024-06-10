70e1b6dce1954d5fb7b16b0d3897461d2433e908
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-10 10:59:41 +0530

    Operations in Javascript

---
+++ 1_Basics/5_conversionOperation.js
@@ -0,0 +1,130 @@
+// let score=33
+
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+//String to number
+
+
+// score ="33"
+
+// console.log(typeof(score));
+
+// console.log("*************************************************************")
+
+// let valueInNumber=Number(score) //conversion
+// console.log(typeof(valueInNumber));
+// console.log("*************************************************************")
+
+
+// score ="33abc"
+// valueInNumber=Number(score)
+// console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log("*************************************************************")
+// score =null
+// valueInNumber=Number(score)
+// console.log(typeof(score));
+// console.log(valueInNumber);
+
+// console.log("*************************************************************")
+// score=undefined
+// valueInNumber=Number(score)
+// console.log(valueInNumber);
+// console.log(typeof(score));
+
+// console.log("*************************************************************")
+
+// score=true
+// valueInNumber=Number(score)
+// console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+
+
+//Note:-
+//"33"=>33
+//"33abcd"=>Na
+//true=>1
+//false=>0
+
+//to boolean
+
+// let isLoggedIn=1
+// let boleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log("*******************************************************************");
+// console.log(boleanIsLoggedIn)
+// console.log(typeof(boleanIsLoggedIn))
+
+// console.log("******************************************************************");
+// isLoggedIn=""
+// boleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(boleanIsLoggedIn)
+// console.log(typeof(boleanIsLoggedIn))
+
+// console.log("******************************************************************");
+// isLoggedIn="Hello"
+// boleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(boleanIsLoggedIn)
+// console.log(typeof(boleanIsLoggedIn))
+
+//1=>true
+//0=>false
+//""=>false
+//"hello"=true
+
+
+
+
+//*********************************************Operation********************************* */
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+
+// let str1="Hello"
+// let str2=" Sarvjeet"
+// let str3=str1+str2
+
+// console.log(str3)
+
+// console.log(1+"2");
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2"+2);
+
+
+
+// console.log(+true)//not prefferd
+// console.log(true+)//Error
+
+// console.log(+""  )
+// let num1,num2,num3   //nnot preffred in Industry
+// num1=num2=num3=100
+// console.table([num1,num2,  num3])
+
+
+// let gameCounter=100
+// gameCounter++;
+// ++gameCounter;
+// console.log(gameCounter)
+
+
+let y = "5";
+let x = + y;
+console.log(x);
+
+
+
+
+
