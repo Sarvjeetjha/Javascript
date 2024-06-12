2edef8e1b124dcc96e63d7652ae00c801376c0ae
Sarvjeetjha <jha.sarvjeet1@gmail.com>
2024-06-12 21:16:50 +0530

    string

---
+++ 1_Basics/8_Strings.js
@@ -0,0 +1,12 @@
+const name="Savrjeet "
+const repoCount=50
+// console.log(name+repoCount + "value");  not  preffered
+console.log(`Hello my name is${name} and my repo count is ${repoCount}  value`);
+
+
+//to declare the string
+const gameName= new String('Sarvjeet kumar')
+console.log(gameName);
+console.log(gameName[0]);
+console.log(gameName[1]);
+console.log(typeof(gameName));
